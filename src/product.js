a218d95874e2fe06037059703148cf553a9610b0
ThomasL <thomas.lefevre@algolia.com>
2021-04-29 18:17:46 +0200

    added objectID variable

--- src/product.js
+++ src/product.js
@@ -1,10 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
+const objectID = urlParams.get('objectID');
 
 const indexName = 'ecommerce_with_settings';
 const client = algoliasearch('9I7OJDGW6A', '92a64db1b9631be9fc3162d1f0467796');
 const index = client.initIndex(indexName);
 
-index.search(urlParams.get('objectID')).then(({ hits }) => {
+index.search(objectID).then(({ hits }) => {
   document.getElementById('product-details').innerHTML = `
     <img src="${hits[0].image}" alt="${hits[0].name}" />
     <div class="product-name">${hits[0].name}</div>
@@ -19,6 +20,6 @@ document.getElementById('add-to-cart').addEventListener('click', () => {
     index: indexName,
     eventName: 'Add to cart',
     queryID: urlParams.get('queryID'),
-    objectIDs: [urlParams.get('objectID')],
+    objectIDs: [objectID],
   });
 });
